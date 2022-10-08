10f1c86c449817d98e8eb118727edede274348c8
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-08 12:36:23 -0300

    feat: add example2 row

---
+++ src/data/example2/customerTable.ts
@@ -0,0 +1,44 @@
+import { IColumn } from "../../types";
+
+export const customersTable: IColumn[] = [
+  {
+    keyName: "name",
+    mode: "crescent",
+    name: "Nome",
+  },
+  {
+    keyName: "country",
+    mode: "crescent",
+    name: "Cidade",
+  },
+  {
+    keyName: "cpf",
+    mode: "crescent",
+    name: "CPF",
+  },
+  {
+    keyName: "profession",
+    mode: "crescent",
+    name: "Profissão",
+  },
+  {
+    keyName: "university",
+    mode: "crescent",
+    name: "Formação",
+  },
+  {
+    keyName: "birthDate",
+    mode: "crescent",
+    name: "Data de nascimento",
+  },
+  {
+    keyName: "status",
+    mode: "crescent",
+    name: "Status",
+  },
+  {
+    keyName: "github",
+    mode: "crescent",
+    name: "Conta github",
+  },
+];
---
+++ src/data/example2/customersData.ts
@@ -0,0 +1,145 @@
+export const customersData = [
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "BAlinsduhue Rodrigues Macedsdihduho",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "10/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "Amazonas",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "02/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+  {
+    name: "Aline Rodrigues Macedo",
+    avatar:
+      "https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg",
+
+    country: "São Paulo",
+    cpf: "526.249.430.72",
+    profession: "Front-end developer",
+    university: "USP - Sistema da Informação",
+    birthDate: "12/09/1998",
+    status: "Working",
+    github: "https://github.com/GuiCoelho-S",
+  },
+];
