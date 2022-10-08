541e549c85c905862419aa7ed9eefc35ea2181b1
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-08 12:35:57 -0300

    feat: create firstRow example

---
+++ src/data/example1/usersTable.ts
@@ -0,0 +1,29 @@
+import { IColumn } from "../../types";
+
+export const usersTable: IColumn[] = [
+  {
+    keyName: "name",
+    name: "Nome",
+    mode: "crescent",
+  },
+  {
+    keyName: "age",
+    name: "Idade",
+    mode: "crescent",
+  },
+  {
+    keyName: "email",
+    name: "Email pessoal",
+    mode: "crescent",
+  },
+  {
+    keyName: "country",
+    name: "Cidade",
+    mode: "crescent",
+  },
+  {
+    keyName: "phone",
+    name: "Celular",
+    mode: "crescent",
+  },
+];
